da2e2ffb02c5c1c109a27bdaa667b80575cfd3df
dbasarena <dbasarena@checkpos.com>
2023-03-12 17:37:28 -0300

    Start screen, directories and classes

---
+++ src/Dependencies.lua
@@ -0,0 +1,17 @@
+-- https://github.com/Ulydev/push
+push = require 'lib/push'
+
+-- https://github.com/vrld/hump/blob/master/class.lua
+Class = require 'lib/class'
+
+require 'src/constants'
+
+require 'src/Paddle'
+
+require 'src/Util'
+
+require 'src/StateMachine'
+
+require 'src/states/BaseState'
+require 'src/states/StartState'
+require 'src/states/PlayState'
